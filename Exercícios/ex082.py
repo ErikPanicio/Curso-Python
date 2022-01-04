d5524f81df5fdffaf3640dd6381a117b44538cc2
ErikP <61640417+ErikPanicio@users.noreply.github.com>
2022-01-04 18:55:50 -0300

    Update ex082.py

--- Exercícios/ex082.py
+++ Exercícios/ex082.py
@@ -8,18 +8,18 @@ impares = []
 while True:
     numeros.append(int(input('Digite um número:')))
     resposta = str(input('Deseja continuar? [S/N]:').upper())
+    if resposta != 'S' and resposta != 'N':
+        print('Valor digitado errado!')
+        del numeros[-1]                 #caso a letra tenha sido digitada errada, o último valor digitado não será armazenado, será deletado, para que o usuário possa digitar novamente o valor e agora com a letra correta [S/N]
     if resposta == 'N':
         for c in range(0, len(numeros)):
             if numeros[c] % 2 == 0:
                 pares.append(numeros[c])
             if numeros[c] % 2 == 1:
                 impares.append(numeros[c])
+        print(f'Números: {numeros}\nPares: {pares}\nÍmpares: {impares}')
         break
-    if resposta != 'S' and resposta != 'N':             #caso o valor seja digitado errado, quero que o usuário possa digitar novamente
-        print('Valor digitado errado!\nDigite novamente:')
 
 
 
-
-print(f'Números: {numeros}\nPares: {pares}\nÍmpares: {impares}')
 
