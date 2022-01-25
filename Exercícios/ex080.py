a7a9494fae8192283f08d646650302ea958df766
ErikP <61640417+ErikPanicio@users.noreply.github.com>
2022-01-25 16:43:24 -0300

    Update ex080.py

--- Exercícios/ex080.py
+++ Exercícios/ex080.py
@@ -2,42 +2,16 @@
 (sem usar o sort()). No final, mostre a lista ordenada na tela.'''
 
 numeros = list()
-cont = 0
 
 for c in range(0, 5):
-    numero = (int(input('Digite um número:')))
-    print(c)
-    if c == 0:
+    numero = int(input('Digite um número:'))
+    if c == 0 or numero > numeros[-1]:
         numeros.append(numero)
-        cont = cont + 1
-    if c == 1:
-        numeros.append(numero)
-        cont = cont + 1
-    elif c == 1 and numeros[c] < numeros[0]:
-        numeros.insert(0, numero)
-        cont = cont + 1
-
-for c in range(0, 5):
-    for v in range(0, 5):       # Não têm mais de 1 ou 2 elementos, então dará erro, já que ele comparará o valor com outro que nem foi digitado ainda
-        if numeros[c] > numeros[v]:
-            numeros.insert(v, numeros[c])
-        if numeros[c] < numeros[v]:
-            numeros.insert(v, numeros[c - 1])
-print(numeros[:])
-
-
-
-
-
-
-'''        
-    if cont == 1 and numeros[c] > numeros[0]:
-        numeros.append(numero)
-        cont = cont + 1
-    if cont == 1 and numeros[c] < numeros[0]:
-        numeros.insert(0, numero)
-        cont = cont + 1
-
-    if numeros[c] > numeros[0]:
-        numeros.append(int(input('Digite um número:')))
-'''
+    else:
+        pos = 0
+        while pos < len(numeros):
+            if numero <= numeros[pos]:
+                numeros.insert(pos, numero)
+                break
+            pos += 1
+print(numeros)
