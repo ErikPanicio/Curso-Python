52474e05569bceb267ccf4ccb51ee883e88d6906
ErikP <61640417+ErikPanicio@users.noreply.github.com>
2022-04-16 19:47:07 -0300

    Update ex087.py

--- Exercícios/ex087.py
+++ Exercícios/ex087.py
@@ -1,7 +1,7 @@
 '''Aprimore o desafio anterior, mostrando no final: A)A soma de todos os valores pares digitados; B)A soma dos valores da terceira coluna;
 C) O maior valor da segunda linha'''
 
-matriz = [ [0, 0, 0], [0, 0, 0], [0, 0, 0] ]
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 soma_par = 0
 soma_impar = 0
 soma_valores = 0
@@ -14,11 +14,11 @@ for c in range(0, 3):
             soma_par += matriz[c][i]
         if matriz[c][i] % 2 == 1:
             soma_impar += matriz[c][i]
-        soma_valores += matriz[2][i]        #Se mudar o valor "2" para "1" o código não funciona. Mas se eu criar uma estrutura "for" a parte, funciona.
         if maior == 0:
             maior = matriz[1][i]
         if matriz[1][i] > maior:
             maior = matriz[1][i]
+    soma_valores += matriz[c][2]
 
 for c in range(0, 3):
     for i in range(0, 3):
